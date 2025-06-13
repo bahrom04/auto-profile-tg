71209184fb071fdeae36e0a82bd4a4b14a8512c5
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-06-13 21:23:01 +0500

    fix: flake scheme + proper devShel

--- default.nix
+++ default.nix
@@ -1,12 +1,11 @@
 # tutorial: https://blog.stigok.com/2019/11/05/packing-python-script-binary-nicely-in-nixos.html
 # reference: https://nixos.org/manual/nixpkgs/stable/#buildpythonpackage-function
-{ 
-fetchFromGitHub
-, python3
-, fetchPypi
-# , python3Packages
+{
+  fetchFromGitHub,
+  python3,
+  fetchPypi,
+  # , python3Packages
 }:
-
 python3.pkgs.buildPythonPackage rec {
   pname = "auto-profile-tg";
   version = "0.0.1";
@@ -15,7 +14,7 @@ python3.pkgs.buildPythonPackage rec {
     owner = "bahrom04";
     repo = "auto-profile-tg";
     rev = "master";
-    sha256 = "sha256-jYhVZ19gAPxSiyosBYECROrC3Vf6kZjZFsBgKFMVBHc=";
+    sha256 = "sha256-vIsCQZqMEap2fF4dsH25Mu6ogbXAIWHv5PCISLS7Q/s=";
   };
 
   propagatedBuildInputs = with python3.pkgs; [
@@ -29,47 +28,47 @@ python3.pkgs.buildPythonPackage rec {
     telethon
     setuptools
   ];
-  
+
   # postPatch = ''
   #   cp ${src}/requirements.txt .
   # '';
 
   preBuild = ''
-    cat > setup.py << EOF
-from setuptools import setup, find_packages
+        cat > setup.py << EOF
+    from setuptools import setup, find_packages
 
-with open('./auto-profile-tg/requirements.txt') as f:
-    install_requires = f.read().splitlines()
+    with open('./auto-profile-tg/requirements.txt') as f:
+        install_requires = f.read().splitlines()
 
-setup(
-  name="auto-profile-tg",
-  packages=find_packages(include=["assets", "prodile_pics", "utils"]),
-  version='0.1.0',
-  #author='...',
-  #description='...',
-  install_requires=[
-    "APScheduler==3.11.0",
-    "loguru==0.7.3",
-    "python-dotenv==1.1.0",
-    "pytz==2025.2",
-    "requests==2.32.4",
-    "Telethon==1.40.0",
-    "tenacity==9.1.2",
-    "setuptools",
-  ],
-  scripts=[
-    './auto-profile-tg/main.py',
-  ],
-  entry_points={
-    # example: file some_module.py -> function main
-    #'console_scripts': ['someprogram=auto-profile-tg:main']
-  },
-)
-EOF
-'';
+    setup(
+      name="auto-profile-tg",
+      packages=find_packages(include=["assets", "prodile_pics", "utils"]),
+      version='0.1.0',
+      #author='...',
+      #description='...',
+      install_requires=[
+        "APScheduler==3.11.0",
+        "loguru==0.7.3",
+        "python-dotenv==1.1.0",
+        "pytz==2025.2",
+        "requests==2.32.4",
+        "Telethon==1.40.0",
+        "tenacity==9.1.2",
+        "setuptools",
+      ],
+      scripts=[
+        './auto-profile-tg/main.py',
+      ],
+      entry_points={
+        # example: file some_module.py -> function main
+        #'console_scripts': ['someprogram=auto-profile-tg:main']
+      },
+    )
+    EOF
+  '';
 
   # No tests
-  doCheck = false; 
+  doCheck = false;
 
   meta = {
     description = "auto-profile adds real-time clock to your telegram profile username";
