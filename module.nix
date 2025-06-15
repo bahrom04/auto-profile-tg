32f1305839ba07d59c67757c0f39e6785a2a9297
bahrom04 <magdiyevbahrom@gmail.com>
2025-06-15 19:54:10 +0500

    fix: secret keys typo

--- module.nix
+++ module.nix
@@ -4,33 +4,33 @@ flake: {
   pkgs,
   ...
 }: let
-  cfg = config.services.auto-profile-tg;
+  cfg = config.services.auto_profile_tg;
   pkg = flake.packages.${pkgs.stdenv.hostPlatform.system}.default;
   homeDir = config.users.users.bahrom04.home;
   
 in {
   options = {
-    services.auto-profile-tg = {
-      enable = lib.mkEnableOption "auto-profile-tg";
+    services.auto_profile_tg = {
+      enable = lib.mkEnableOption "auto_profile_tg";
 
       # Telegram API credentials from my.telegram.org
-      api-id = lib.mkOption {
+      api_id = lib.mkOption {
         type = lib.types.int;
         default = 0;
         example = 12345678;
       };
 
-      api-hash = lib.mkOption {
+      api_hash = lib.mkOption {
         type = lib.types.str;
         default = "";
       };
 
-      phone-number = lib.mkOption {
+      phone_number = lib.mkOption {
         type = lib.types.str;
         example = "+998123456789";
       };
 
-      first-name = lib.mkOption {
+      first_name = lib.mkOption {
         type = lib.types.str;
         example = "John";
       };
@@ -55,7 +55,7 @@ in {
         example = "Tashkent";
       };
 
-      weather-api-key = lib.mkOption {
+      weather_api_key = lib.mkOption {
         type = lib.types.str;
       };
     };
@@ -78,23 +78,23 @@ in {
 
     # I use mac btw
     launchd.user.agents = {
-      auto-profile-tg = {
+      auto_profile_tg = {
         serviceConfig = {
           # Program = "${pkg}/bin/runner";
           ProgramArguments = [
             "${pkg}/bin/runner" 
-            "--api_id=${cfg.api-id}" 
-            "--api_hash=${cfg.api-hash}" 
-            "--phone_number=${cfg.phone-number}" 
-            "--first_name=${cfg.first-name}"
+            "--api_id=${cfg.api_id}" 
+            "--api_hash=${cfg.api_hash}" 
+            "--phone_number=${cfg.phone_number}" 
+            "--first_name=${cfg.first_name}"
             "--lat=${cfg.lat}"
             "--lon=${cfg.lon}"
             "--timezone=${cfg.timezone}"
             "--city=${cfg.city}"
-            "--weather_api_key=${cfg.weather-api-key}"
+            "--weather_api_key=${cfg.weather_api_key}"
             ]; 
-          StandardOutPath = "/${homeDir}/Library/Logs/auto-profile-tg.log";
-          StandardErrorPath = "/${homeDir}/Library/Logs/auto-profile-tg-error.log";
+          StandardOutPath = "/${homeDir}/Library/Logs/auto_profile_tg.log";
+          StandardErrorPath = "/${homeDir}/Library/Logs/auto_profile_tg_error.log";
           KeepAlive = true;
           RunAtLoad = true;
         };
